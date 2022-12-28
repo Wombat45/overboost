cdd931ddfcf8292ebb9729650fb6e51af545b4f2
mike <nazzzjr20@gmail.com>
2022-12-28 15:31:43 -0500

    test push

--- overboost/Pages/Index.cshtml.cs
+++ overboost/Pages/Index.cshtml.cs
@@ -7,11 +7,12 @@ namespace overboost.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
         public JsonFileCarsService CarsService { get; }
         public IEnumerable<Car>? Cars { get; private set; }
 
         public IndexModel(
-            
+
             ILogger<IndexModel> logger,
             JsonFileCarsService carsService)
         {
